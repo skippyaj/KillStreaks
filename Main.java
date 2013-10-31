26f5c8c6b2e37afcc2dbb9f14f3c8f844484f108
xxMOxMOxx <mematt87@gmail.com>
2013-10-31 09:52:46 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,4 +2,31 @@ public class Main extends JavaPlugin implements Listener {
     public void onEnable(){
         getServer().getPluginManager().registerEvents(this, this);
     }
+    @EventHandler
+    public void onHit(EntityDamageByEntityEvent event){
+        if(event.getEntity() instanceof Player && event.getDamager() instanceof Player){
+            Player damager = (Player) event.getDamager();
+            
+            //InstaKill with gold sword
+            if(damager.getItemInHand()!=null && damager.getItemInHand().getType()==Material.GOLD_SWORD){
+                event.setDamage(100);
+                damager.setItemInHand(null);
+            }
+            
+            //no damage in creative
+            if(damager.isInCreative()){
+                event.setCancelled(true);
+            }
+        }
+    }
+    
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event){
+        if(event.getBlockPlace().getType()==Material.OBSIDIAN){
+            if(!event.getPlayer().isInCreative()){
+                event.setCancelled(true);
+                //activate special ability
+            }
+        }
+    }
 }
