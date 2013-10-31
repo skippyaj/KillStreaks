9d520dcd0ac44606e5b33d2257b70be8a6ae385e
xxMOxMOxx <mematt87@gmail.com>
2013-10-31 12:04:50 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,25 @@
+package killstreaks;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.String;
+
 public class Main extends JavaPlugin implements Listener {
+    
+    public HashMap<String, Integer> kills = new HashMap<String, Integer>();
+    
+    
     public void onEnable(){
         getServer().getPluginManager().registerEvents(this, this);
     }
+    
+    
     @EventHandler
     public void onHit(EntityDamageByEntityEvent event){
         if(event.getEntity() instanceof Player && event.getDamager() instanceof Player){
@@ -12,7 +30,6 @@ public class Main extends JavaPlugin implements Listener {
                 event.setDamage(100);
                 damager.setItemInHand(null);
             }
-            
             //no damage in creative
             if(damager.getGameMode()==GameMode.CREATIVE){
                 event.setCancelled(true);
@@ -22,11 +39,26 @@ public class Main extends JavaPlugin implements Listener {
     
     @EventHandler
     public void onPlace(BlockPlaceEvent event){
-        Player player = event,getPlayer();
-        if(event.getBlockPlace().getType()==Material.OBSIDIAN){
+        Player player = event.getPlayer();
+        if(event.getBlockPlaced().getType()==Material.OBSIDIAN){
             if(!player.getGameMode()==GameMode.CREATIVE){
-                event.setCancelled(true);
                 player.setItemInHand(null);
+                //activate special power
+                event.setCancelled(true);
+            }
+        }
+    }
+    
+    
+    @EventHandler
+    public void onDeath(PlayerDeathEvent event) {
+        kills.put(event.getEntity().getName(), 0);
+        if(event.getKiller()!=null){
+            Player killer = event.getEntity().getKiller();
+            if(kills.contains(killer.getName)){
+                kills.put(killer.getName(), kills.get(killer.getName()+1);
+            }else{
+                kills.put(killer.getName(), 1);
             }
         }
     }
